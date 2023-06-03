0815c47234f39ed09ed7c79e651ee7ebcb7bb8c4
Sanjay Shrestha <sanjayshrestha969@gmail.com>
2023-06-03 16:14:51 +0545

    loading screen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,6 +18,7 @@ export default function Home() {
   };
 
   const [searchResults, setSearchResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const [pageNumber, setPageNumber] = useState<number>(1)
   const [keyword, setSearchKeyword] = useState<string>('')
@@ -30,9 +31,11 @@ export default function Home() {
       })
       .then(data => {
         setSearchResults(data);
-        console.log("response", data);
+        setIsLoading(false)
       }).catch((err) => {
         console.log(err)
+        setIsLoading(false)
+
       });
   };
 
@@ -40,12 +43,14 @@ export default function Home() {
 
   useEffect(() => {
     if (keyword) {
+      setIsLoading(true)
       getSearchResult(keyword)
     }
   }, [keyword, pageNumber])
 
   const handleSubmit = (values: FormValues) => {
     if (values.keyword) {
+      setIsLoading(true)
       setPageNumber(1)
       setSearchKeyword(values.keyword)
       dispatch(setKeyword(values.keyword));
@@ -73,6 +78,17 @@ export default function Home() {
     <main
       className={`pb-20`}
     >
+      {
+        isLoading ?
+          <div className="min-h-full min-w-full bg-gray-300 fixed top-0 z-50 left-0 bg-opacity-40 grid place-items-center">
+            <div className="flex gap-4 items-center">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2A10 10 0 1 0 22 12A10 10 0 0 0 12 2Zm0 18a8 8 0 1 1 8-8A8 8 0 0 1 12 20Z" opacity=".5" /><path fill="currentColor" d="M20 12h2A10 10 0 0 0 12 2V4A8 8 0 0 1 20 12Z"><animateTransform attributeName="transform" dur="1s" from="0 12 12" repeatCount="indefinite" to="360 12 12" type="rotate" /></path></svg>
+              <label className="font-semibold"> Processing</label>
+            </div>
+          </div>
+          :
+          ""
+      }
       <section className='flex flex-col gap-10 p-20 pb-10 place-items-center bg-brand-4 px-10 '>
         <Image alt='logo' width={'200'} height={'40'} src={"/cu.svg"} priority={true} style={{ width: "auto", height: "auto", maxWidth: "200px" }} />
         <div className='relative w-full flex bg-white container overflow-hidden rounded-md '>
@@ -110,6 +126,7 @@ export default function Home() {
           <div className='flex justify-between items-center container mx-auto px-10'>
             <button disabled={pageNumber === 1}
               onClick={() => {
+                setIsLoading(true)
                 setPageNumber(pageNumber - 1)
               }} className={`flex items-center gap-2 px-6 py-2 bg-brand-3 rounded-md text-white ${pageNumber === 1 && "bg-gray-300"
                 }`}> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none"><path d="M24 0v24H0V0h24ZM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018Zm.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022Zm-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01l-.184-.092Z" /><path fill="currentColor" d="M3.636 11.293a1 1 0 0 0 0 1.414l5.657 5.657a1 1 0 0 0 1.414-1.414L6.757 13H20a1 1 0 1 0 0-2H6.757l3.95-3.95a1 1 0 0 0-1.414-1.414l-5.657 5.657Z" /></g></svg> Previous</button>
@@ -122,6 +139,7 @@ export default function Home() {
             <button
               disabled={pageNumber === Math.ceil(searchResults?.totalData / pageLimit)}
               onClick={() => {
+                setIsLoading(true)
                 setPageNumber(pageNumber + 1)
               }} className={`flex items-center gap-2 px-6 py-2 bg-brand-3 rounded-md text-white ${pageNumber === Math.ceil(searchResults?.totalData / pageLimit) && "bg-gray-300"
                 }`}>Next <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 12l-6-6m6 6l-6 6m6-6H5" /></svg></button>
